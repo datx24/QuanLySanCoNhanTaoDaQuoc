cfdb2709930964162e1856dba0849e75d431c530
datx24 <xuandat475@gmail.com>
2024-11-27 03:31:57 +0700

    cai dat lop field DTO

---
+++ QLSanBongDaQuoc_64130299/src/main/java/DTO/FieldDTO.java
@@ -0,0 +1,63 @@
+package DTO;
+
+public class FieldDTO {
+	private int fieldID;            // ID duy nhất cho mỗi sân bóng
+    private String fieldName;       // Tên sân bóng
+    private String status;          // Trạng thái của sân bóng (có sẵn, bảo trì, đă đặt)
+    private double pricePerHour;    // Giá thuê sân mỗi giờ
+    private String description;     // Mô tả sân bóng
+    
+	public FieldDTO() {
+	}
+
+	public FieldDTO(int fieldID, String fieldName, String status, double pricePerHour, String description) {
+		super();
+		this.fieldID = fieldID;
+		this.fieldName = fieldName;
+		this.status = status;
+		this.pricePerHour = pricePerHour;
+		this.description = description;
+	}
+
+	public int getFieldID() {
+		return fieldID;
+	}
+
+	public void setFieldID(int fieldID) {
+		this.fieldID = fieldID;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public double getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+    
+}
