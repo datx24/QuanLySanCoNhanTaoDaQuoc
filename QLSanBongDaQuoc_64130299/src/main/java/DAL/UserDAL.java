6f1ad089585d16be106dc46df4f12ca4ef557110
datx24 <xuandat475@gmail.com>
2024-11-27 03:24:31 +0700

    cài đặt thêm các phương thức trong UserDTO

--- QLSanBongDaQuoc_64130299/src/main/java/DAL/UserDAL.java
+++ QLSanBongDaQuoc_64130299/src/main/java/DAL/UserDAL.java
@@ -1,5 +1,36 @@
 package DAL;
 
-public class UserDAL {
+import DTO.UserDTO;
 
+public class UserDAL {
+	// thêm người dùng
+	public boolean insertUser(UserDTO user) {
+		return true;
+	}
+
+	// xóa người dùng
+	public boolean updateUser(UserDTO user) {
+		return true;
+	}
+	
+	// sửa thông tin người dùng
+	public boolean editUser(UserDTO user) {
+		return true;
+	}
+
+	// cập nhật người dùng
+	public boolean deleteUser(UserDTO user) {
+		return true;
+	}
+
+	// đăng nhập
+	public boolean login(String email,String passwordHash) {
+		return true;
+	}
+
+	// đăng kí
+	public boolean register(String fullName,String email,String username,
+			String password,String phoneNumber,String passwordHash) {
+		return true;
+	}
 }
