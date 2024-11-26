3cc5c1b575f3dafcb22f6978e6c83c311b1de06d
datx24 <xuandat475@gmail.com>
2024-11-27 06:18:38 +0700

    liệt kê các phương thức trong MaintenaceDAL

---
+++ QLSanBongDaQuoc_64130299/src/main/java/DAL/MaintenanceDAL.java
@@ -0,0 +1,63 @@
+package DAL;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import DTO.MaintenanceDTO;
+
+public class MaintenanceDAL {
+	private MaintenanceDTO maintenanceDTO = new MaintenanceDTO();
+	private List<MaintenanceDTO> maintenanceDTOs = new ArrayList<>();
+
+	// Thêm lịch bảo trì
+	public boolean addMaintenance(MaintenanceDTO maintenance) {
+		return true;
+	}
+
+	// Cập nhật thông tin bảo trì
+	public boolean updateMaintenance(MaintenanceDTO maintenance) {
+		return true;
+	}
+
+	// Xóa lịch bảo trì
+	public boolean deleteMaintenance(MaintenanceDTO maintenance) {
+		return true;
+	}
+
+	// Lấy thông tin bảo trì theo ID
+	public MaintenanceDTO getMaintenanceByID(String maintenanceID) {
+		return maintenanceDTO;
+	}
+
+	// Lấy tất cả lịch bào trì
+	public List<MaintenanceDTO> getAllMaintenance() {
+		return maintenanceDTOs;
+	}
+
+	// Lấy lịch bảo trì theo sân
+	public List<MaintenanceDTO> getMaintenanceByFieldID(int fieldID) {
+		return maintenanceDTOs;
+	}
+
+	// Kiểm tra lịch bảo trì có bị trùng hay không
+	public boolean checkMaintenanceOverlap(int fieldID, Date startDate, Date endDate) {
+		return true;
+	}
+	
+	// Lấy tất cả lịch bảo trì theo thời gian
+	public List<MaintenanceDTO> getMaintenanceByDateRange(Date startDate, Date endDate){
+		return maintenanceDTOs;
+	}
+	
+	// Lấy lịch bảo trì sắp tới
+	public List<MaintenanceDTO> getUpcomingMaintenance(){
+		return maintenanceDTOs;
+	}
+	
+	// Tìm kiếm lịch bảo trì theo mô tả
+	public List<MaintenanceDTO> searchMaintenanceByDescription(String keyword){
+		return maintenanceDTOs;
+	}
+
+}
