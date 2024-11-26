03991f0b2dfeb14a41158197cdfa466411842b7e
datx24 <xuandat475@gmail.com>
2024-11-27 05:16:50 +0700

    liệt kê các phương thức cần cài đặt trong Invoices

---
+++ QLSanBongDaQuoc_64130299/src/main/java/DAL/InvoiceDAL.java
@@ -0,0 +1,46 @@
+package DAL;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import DTO.InvoiceDTO;
+
+public class InvoiceDAL {
+	//Thêm hóa đơn thanh toán
+	public boolean addInvoice(InvoiceDTO invoice) {
+		return true;
+	}
+	
+	//Cập nhật hóa đơn thanh toán
+	public boolean updateInvoice(InvoiceDTO invoice) {
+		return true;
+	}
+	
+	//Xóa hóa đơn thanh toán
+	public boolean deleteInvoice(InvoiceDTO invoice) {
+		return true;
+	}
+	
+	//Lấy danh sách tất cả hóa đơn từ cơ sở dữ liệu 
+	public List<InvoiceDTO> getAllInvoices(InvoiceDTO invoice) {
+		List<InvoiceDTO> invoices = new ArrayList<InvoiceDTO>();
+		return invoices;
+	}
+	
+	//Lấy hóa đơn theo ID
+	public InvoiceDTO getInvoiceByID(String invoiceID) {
+		InvoiceDTO invoice = new InvoiceDTO();
+		return invoice;
+	}
+	
+	//Lấy hóa đơn dựa trên BookingID
+	public InvoiceDTO getInvoiceByBookingID(String bookingID) {
+		InvoiceDTO invoice = new InvoiceDTO();
+		return invoice;
+	}
+	
+	//Phương thức kiểm tra trạng thái thanh toán của hóa đơn 
+	public PaymentStatus checkPaymentStatus(String invoiceID) {
+		return null;
+	}
+} 
