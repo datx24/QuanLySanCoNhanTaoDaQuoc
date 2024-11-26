7233c817f4f0ed24ff877d114e7e21534b6714af
datx24 <xuandat475@gmail.com>
2024-11-27 04:46:42 +0700

    cài đặt lớp field status

---
+++ QLSanBongDaQuoc_64130299/src/main/java/Enum/FieldStatus.java
@@ -0,0 +1,27 @@
+package Enum;
+
+public enum FieldStatus {
+    AVAILABLE("Available"),//có sẵn
+    MAINTENANCE("Maintenance"),//bảo trì
+    BOOKED("Booked");//đã đặt
+
+    private final String status;
+
+    FieldStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public static FieldStatus fromString(String status) {
+        for (FieldStatus fs : FieldStatus.values()) {
+            if (fs.getStatus().equalsIgnoreCase(status)) {
+                return fs;
+            }
+        }
+        throw new IllegalArgumentException("Unknown status: " + status);
+    }
+}
+
