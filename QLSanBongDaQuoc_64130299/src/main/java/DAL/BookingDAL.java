e87c61c7b59e8fa9eadd1ca7a753d0ecc39e254e
datx24 <xuandat475@gmail.com>
2024-11-27 04:55:13 +0700

    thêm phương thức lấy tất cả đơn đặt sân từ cơ sở dữ liệu

--- QLSanBongDaQuoc_64130299/src/main/java/DAL/BookingDAL.java
+++ QLSanBongDaQuoc_64130299/src/main/java/DAL/BookingDAL.java
@@ -7,29 +7,33 @@ import DTO.BookingDTO;
 import Enum.BookingStatus;
 
 public class BookingDAL {
-	//Thêm đơn đặt sân
+	// Thêm đơn đặt sân
 	public boolean addBooking(BookingDTO booking) {
 		return true;
 	}
-	
-	//Cập nhật đơn đặt sân
+
+	// Cập nhật đơn đặt sân
 	public boolean updateBooking(BookingDTO booking) {
 		return true;
 	}
-	
-	//Xóa đơn đặt sân
+
+	// Xóa đơn đặt sân
 	public boolean deleteBooking(String bookingID) {
 		return true;
 	}
-	
-	
-	//Lấy danh sách đặt sân theo trạng thái
+
+	// Lấy tất cả đơn đặt sân từ cơ sở dữ liệu
+	public boolean getAllBookings() {
+		return true;
+	}
+
+	// Lấy danh sách đặt sân theo trạng thái
 	public List<BookingDTO> getBookingByStatus(BookingStatus booking) {
 		List<BookingDTO> bookings = new ArrayList<BookingDTO>();
 		return bookings;
 	}
-	
-	//Lấy thông tin của 1 đơn đặt sân theo id
+
+	// Lấy thông tin của 1 đơn đặt sân theo id
 	public BookingDTO getBookingById(String bookingId) {
 		BookingDTO booking = null;
 		return booking;
