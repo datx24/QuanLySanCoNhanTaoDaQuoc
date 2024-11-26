7d2391949c24034ad9cad3fab6711027943b98d7
datx24 <xuandat475@gmail.com>
2024-11-27 03:02:44 +0700

    Cài đặt UserDTO

---
+++ QLSanBongDaQuoc_64130299/src/main/java/DTO/UserDTO.java
@@ -0,0 +1,10 @@
+package DTO;
+
+public class UserDTO {
+	private String UserID;
+	private String FullName;
+	private String Email;
+	private String PhoneNumber;
+	private String PasswordHash;
+	private boolean IsAdmin;
+}
