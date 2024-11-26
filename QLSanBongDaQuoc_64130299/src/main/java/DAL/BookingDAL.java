efe82763bfa1424b915a99a422a34bd6f9679b95
datx24 <xuandat475@gmail.com>
2024-11-27 04:44:18 +0700

    thêm enum booking status

--- QLSanBongDaQuoc_64130299/src/main/java/DAL/BookingDAL.java
+++ QLSanBongDaQuoc_64130299/src/main/java/DAL/BookingDAL.java
@@ -1,5 +1,37 @@
 package DAL;
 
-public class BookingDAL {
+import java.util.ArrayList;
+import java.util.List;
 
+import DTO.BookingDTO;
+import Enum.BookingStatus;
+
+public class BookingDAL {
+	//Thêm đơn đặt sân
+	public boolean addBooking(BookingDTO booking) {
+		return true;
+	}
+	
+	//Cập nhật đơn đặt sân
+	public boolean updateBooking(BookingDTO booking) {
+		return true;
+	}
+	
+	//Xóa đơn đặt sân
+	public boolean deleteBooking(String bookingID) {
+		return true;
+	}
+	
+	
+	//Lấy danh sách đặt sân theo trạng thái
+	public List<BookingDTO> getBookingByStatus(BookingStatus booking) {
+		List<BookingDTO> bookings = new ArrayList<BookingDTO>();
+		return bookings;
+	}
+	
+	//Lấy thông tin của 1 đơn đặt sân theo id
+	public BookingDTO getBookingById(String bookingId) {
+		BookingDTO booking = null;
+		return booking;
+	}
 }
