6932e4007da50e1b11a1444780959505c38799c2
datx24 <xuandat475@gmail.com>
2024-11-27 22:19:31 +0700

    cài đặt login trong user dal

--- QLSanBongDaQuoc_64130299/src/main/java/DAL/UserDAL.java
+++ QLSanBongDaQuoc_64130299/src/main/java/DAL/UserDAL.java
@@ -1,14 +1,20 @@
 package DAL;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 import DTO.UserDTO;
+import Utils.DatabaseConnection;
 
 public class UserDAL {
 	private UserDTO userDTO = new UserDTO();
 	private List<UserDTO> userDTOs = new ArrayList();
+
 	// thêm người dùng
 	public boolean insertUser(UserDTO user) {
 		return true;
@@ -28,12 +34,12 @@ public class UserDAL {
 	public List<UserDTO> getAllUsers() {
 		return userDTOs;
 	}
-	
+
 	// Lấy thông tin người dùng dựa vào ID
 	public UserDTO getUserByID(String UserID) {
 		return userDTO;
 	}
-	
+
 	// Lấy thông tin người dùng dựa vào email
 	public UserDTO getUserByEmail(String email) {
 		return userDTO;
@@ -41,13 +47,27 @@ public class UserDAL {
 
 	// đăng nhập
 	public boolean login(String email, String passwordHash) {
-		return true;
+		String sql = "SELECT COUNT(*) FROM users_64130299 WHERE email = ? AND password = ?";
+		try (Connection conn = DatabaseConnection.getConnection(); // Kết nối từ DatabaseConnection
+				PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+			stmt.setString(1, email);
+			stmt.setString(2, passwordHash);
+
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next() && rs.getInt(1) > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 	// đăng kí
-	public boolean register(String fullName, String email, String phoneNumber, String passwordHash, 
-            Timestamp createdAt, Timestamp updatedAt) {
+	public boolean register(String fullName, String email, String phoneNumber, String passwordHash, Timestamp createdAt,
+			Timestamp updatedAt) {
 		return true;
 	}
-	
+
 }
