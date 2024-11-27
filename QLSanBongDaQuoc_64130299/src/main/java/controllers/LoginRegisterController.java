961b70d3380daa16f107bf1ea8ddb58e3e87089f
datx24 <xuandat475@gmail.com>
2024-11-27 22:04:26 +0700

    viết các thuộc tính và liệt kê hai phương thức đăng nhập, đăng kí

--- QLSanBongDaQuoc_64130299/src/main/java/controllers/LoginRegisterController.java
+++ QLSanBongDaQuoc_64130299/src/main/java/controllers/LoginRegisterController.java
@@ -1,5 +1,46 @@
 package controllers;
 
-public class LoginRegisterController {
+import BLL.UserBLL;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginRegisterController {
+	@FXML
+    private TextField usernameField; // TextField nhập tên đăng nhập
+    @FXML
+    private PasswordField passwordField; // PasswordField nhập mật khẩu
+    @FXML
+    private Button loginButton; // Nút đăng nhập
+    @FXML
+    private TextField fullNameField; // TextField nhập họ và tên (dành cho đăng ký)
+    @FXML
+    private TextField emailField; // TextField nhập email (dành cho đăng ký)
+    @FXML
+    private TextField phoneField; // TextField nhập số điện thoại (dành cho đăng ký)
+    @FXML
+    private PasswordField registerPasswordField; // PasswordField mật khẩu (dành cho đăng ký)
+    @FXML
+    private PasswordField confirmPasswordField; // Xác nhận mật khẩu (dành cho đăng ký)
+    @FXML
+    private Button registerButton; // Nút đăng ký
+    
+    private UserBLL userBLL = new UserBLL(); 
+    
+    // Xử lý đăng nhập
+    @FXML
+    public void handleLogin() {
+    	
+    }
+    
+    // Xử lý đăng kí
+    @FXML
+    public void handleRegister() {
+    	
+    }
+    
 }
