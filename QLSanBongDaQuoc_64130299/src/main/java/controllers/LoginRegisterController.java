a3970e70a084cf2cce3ad39499aac1c87742d3f6
datx24 <xuandat475@gmail.com>
2024-11-27 22:11:41 +0700

    cài đặt phương thức alert và đăng nhập

--- QLSanBongDaQuoc_64130299/src/main/java/controllers/LoginRegisterController.java
+++ QLSanBongDaQuoc_64130299/src/main/java/controllers/LoginRegisterController.java
@@ -11,36 +11,55 @@ import javafx.scene.control.TextField;
 
 public class LoginRegisterController {
 	@FXML
-    private TextField usernameField; // TextField nhập tên đăng nhập
-    @FXML
-    private PasswordField passwordField; // PasswordField nhập mật khẩu
-    @FXML
-    private Button loginButton; // Nút đăng nhập
-    @FXML
-    private TextField fullNameField; // TextField nhập họ và tên (dành cho đăng ký)
-    @FXML
-    private TextField emailField; // TextField nhập email (dành cho đăng ký)
-    @FXML
-    private TextField phoneField; // TextField nhập số điện thoại (dành cho đăng ký)
-    @FXML
-    private PasswordField registerPasswordField; // PasswordField mật khẩu (dành cho đăng ký)
-    @FXML
-    private PasswordField confirmPasswordField; // Xác nhận mật khẩu (dành cho đăng ký)
-    @FXML
-    private Button registerButton; // Nút đăng ký
-    
-    private UserBLL userBLL = new UserBLL(); 
-    
-    // Xử lý đăng nhập
-    @FXML
+	private TextField usernameField; // TextField nhập tên đăng nhập
+	@FXML
+	private PasswordField passwordField; // PasswordField nhập mật khẩu
+	@FXML
+	private Button loginButton; // Nút đăng nhập
+	@FXML
+	private TextField fullNameField; // TextField nhập họ và tên (dành cho đăng ký)
+	@FXML
+	private TextField emailField; // TextField nhập email (dành cho đăng ký)
+	@FXML
+	private TextField phoneField; // TextField nhập số điện thoại (dành cho đăng ký)
+	@FXML
+	private PasswordField registerPasswordField; // PasswordField mật khẩu (dành cho đăng ký)
+	@FXML
+	private PasswordField confirmPasswordField; // Xác nhận mật khẩu (dành cho đăng ký)
+	@FXML
+	private Button registerButton; // Nút đăng ký
+
+	private UserBLL userBLL = new UserBLL();
+
+	// Xử lý đăng nhập
+	@FXML
     public void handleLogin() {
+    	String username = usernameField.getText();
+    	String password = passwordField.getText();
     	
-    }
-    
-    // Xử lý đăng kí
-    @FXML
-    public void handleRegister() {
+    	boolean loginSuccess = userBLL.checkLogin(username, password);
     	
+    	if(loginSuccess) {
+    		//Hiển thị thông báo,sau này cài đặt để chuyển màn hình khác
+    		showAlert("Thông báo", "Đăng ký thành công!", AlertType.INFORMATION);
+    	} else {
+    		showAlert("Lỗi", "Đăng ký thất bại!", AlertType.ERROR);
+    	}
     }
-    
+
+	// Xử lý đăng kí
+	@FXML
+	public void handleRegister() {
+
+	}
+
+	// Phương thức hiển thị thông báo
+	private void showAlert(String title, String message, AlertType alertType) {
+		Alert alert = new Alert(alertType);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(message);
+		alert.showAndWait();
+	}
+
 }
