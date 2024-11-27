b4aa018ea5eccd557a0ecff24114aa72bf5f11e4
datx24 <xuandat475@gmail.com>
2024-11-27 21:38:18 +0700

    chỉnh code hàm main và giao diện

--- QLSanBongDaQuoc_64130299/src/main/java/Test/Main.java
+++ QLSanBongDaQuoc_64130299/src/main/java/Test/Main.java
@@ -1,21 +1,38 @@
 package Test;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 import Utils.DatabaseConnection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/LoginRegisterView.fxml")); 
+        primaryStage.setTitle("Quản Lý Sân Bóng");
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(800);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+	
 	  public static void main(String[] args) {
-	        try (Connection connection = DatabaseConnection.getConnection()) {
-	            if (connection != null) {
-	                System.out.println("Kết nối cơ sở dữ liệu thành công!");
-	            } else {
-	                System.out.println("Kết nối cơ sở dữ liệu thất bại!");
-	            }
-	        } catch (SQLException e) {
-	            System.err.println("Đã xảy ra lỗi khi kết nối cơ sở dữ liệu:");
-	            e.printStackTrace();
-	        }
+//	        try (Connection connection = DatabaseConnection.getConnection()) {
+//	            if (connection != null) {
+//	                System.out.println("Kết nối cơ sở dữ liệu thành công!");
+//	            } else {
+//	                System.out.println("Kết nối cơ sở dữ liệu thất bại!");
+//	            }
+//	        } catch (SQLException e) {
+//	            System.err.println("Đã xảy ra lỗi khi kết nối cơ sở dữ liệu:");
+//	            e.printStackTrace();
+//	        }
+		  launch(args);
 	    }
 }
