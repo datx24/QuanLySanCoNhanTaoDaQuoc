e1edc074342e6236c63b63c26ed28e43163bd750
datx24 <xuandat475@gmail.com>
2024-11-27 19:22:08 +0700

    liệt kê các phương thức cần cài đặt trong lớp BLL maintenance

---
+++ QLSanBongDaQuoc_64130299/src/main/java/BLL/MaintenanceBLL.java
@@ -0,0 +1,59 @@
+package BLL;
+
+import DTO.MaintenanceDTO;
+
+import java.sql.Date;
+import java.util.List;
+
+import DAL.MaintenanceDAL;
+
+public class MaintenanceBLL {
+	private MaintenanceDAL maintenanceDAL = new MaintenanceDAL();
+	
+	//Xử lý logic trước khi thêm lịch bảo trì
+	public boolean addMaintenanceIfNotOverlap(MaintenanceDTO maintenanceDTO) {
+		//Cài đặt xử lý trước khi thêm
+		return maintenanceDAL.addMaintenance(maintenanceDTO);
+	}
+	
+	//Cập nhật thông tin bảo trì
+	public boolean updateMaintenance(MaintenanceDTO maintenanceDTO) {
+		return maintenanceDAL.updateMaintenance(maintenanceDTO);
+	}
+	
+	//Xóa lịch bảo trì
+	public boolean deleteMaintenance(String maintenanceID) {
+		MaintenanceDTO maintenanceDTO = getMaintenanceByID(maintenanceID);
+		return maintenanceDAL.deleteMaintenance(maintenanceDTO);
+	}
+	
+	//Lấy thông tin bảo trì theo ID
+	public MaintenanceDTO getMaintenanceByID(String maintenanceID) {
+		return maintenanceDAL.getMaintenanceByID(maintenanceID);
+	}
+	
+	//Lấy tất cả lịch bảo trì
+	public List<MaintenanceDTO> getAllMaintenance() {
+		return maintenanceDAL.getAllMaintenance();
+	}
+	
+	//Lấy lịch bảo trì theo sân
+	public List<MaintenanceDTO> getMaintenanceByFieldID(int fieldID) {
+		return maintenanceDAL.getMaintenanceByFieldID(fieldID);
+	}
+	
+	//Kiểm tra lịch bảo trì có trùng không
+	public boolean checkMaintenanceOverlap(int fieldID, Date startDate, Date endDate) {
+		return maintenanceDAL.checkMaintenanceOverlap(fieldID, startDate, endDate);
+	}
+	
+	//Lấy tất cả lịch bảo trì theo thời gian
+	public List<MaintenanceDTO> getMaintenanceByDateRange(Date startDate, Date endDate) {
+        return maintenanceDAL.getMaintenanceByDateRange(startDate, endDate);
+    }
+	
+	// Lấy lịch bảo trì sắp tới
+    public List<MaintenanceDTO> getUpcomingMaintenance() {
+        return maintenanceDAL.getUpcomingMaintenance();
+    }
+}
