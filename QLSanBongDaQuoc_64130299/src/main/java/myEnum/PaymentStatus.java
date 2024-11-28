89837cf4b4c63417d1c95a9e06b9963270f37a6e
datx24 <xuandat475@gmail.com>
2024-11-28 11:09:37 +0700

    thêm enum cho trạng thái thanh toán

---
+++ QLSanBongDaQuoc_64130299/src/main/java/myEnum/PaymentStatus.java
@@ -0,0 +1,30 @@
+package myEnum;
+
+public enum PaymentStatus {
+    PAID("Paid"),    // Đã thanh toán
+    UNPAID("Unpaid"); // Chưa thanh toán
+
+    private final String status;
+
+    PaymentStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return this.status;
+    }
+
+    public static PaymentStatus fromString(String status) {
+        for (PaymentStatus paymentStatus : PaymentStatus.values()) {
+            if (paymentStatus.status.equalsIgnoreCase(status)) {
+                return paymentStatus;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant with status " + status);
+    }
+}
