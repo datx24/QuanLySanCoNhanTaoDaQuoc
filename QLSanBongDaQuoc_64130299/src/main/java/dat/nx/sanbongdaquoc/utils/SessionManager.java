9b7b01b2b9fbfbb149e908d8b70f7e70e8543870
datx24 <xuandat475@gmail.com>
2024-11-30 11:37:14 +0700

    Cài đặt lớp quản lí trạng thái đăng nhập đăng xuất

---
+++ QLSanBongDaQuoc_64130299/src/main/java/dat/nx/sanbongdaquoc/utils/SessionManager.java
@@ -0,0 +1,25 @@
+package dat.nx.sanbongdaquoc.utils;
+
+import dat.nx.sanbongdaquoc.DTO.UserDTO;
+
+public class SessionManager {
+	private static UserDTO currentUser;
+	//Lấy thông tin người dùng đang đăng nhập
+	public static UserDTO getCurrentUser() {
+		return currentUser;
+	}
+	//Gán thông tin người dùng đang đăng nhập
+	public static void setCurrentUser(UserDTO currentUser) {
+		SessionManager.currentUser = currentUser;
+	}
+	
+	//Xóa thông tin phiên đăng nhập
+	public static void logout() {
+		currentUser = null;
+	}
+	
+	//Kiểm tra trạng thái đăng nhập
+	public static boolean isLoggedIn() {
+		return currentUser != null;
+	}
+}
