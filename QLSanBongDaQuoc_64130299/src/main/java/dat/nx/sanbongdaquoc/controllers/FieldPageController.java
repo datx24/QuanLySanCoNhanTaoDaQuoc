8fee7f4672508f3b095d0bc62639108396ffa14f
datx24 <xuandat475@gmail.com>
2024-12-17 18:22:07 +0700

    cài đặt code hiển thị bảng và combobox

--- QLSanBongDaQuoc_64130299/src/main/java/dat/nx/sanbongdaquoc/controllers/FieldPageController.java
+++ QLSanBongDaQuoc_64130299/src/main/java/dat/nx/sanbongdaquoc/controllers/FieldPageController.java
@@ -1,9 +1,14 @@
 package dat.nx.sanbongdaquoc.controllers;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import dat.nx.sanbongdaquoc.enums.FieldStatus;
 import dat.nx.sanbongdaquoc.models.entities.FieldDTO;
+import dat.nx.sanbongdaquoc.repositories.FieldDAL;
+import dat.nx.sanbongdaquoc.services.FieldBLL;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -13,6 +18,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -53,9 +59,63 @@ public class FieldPageController {
 	@FXML
 	private TableView<FieldDTO> fieldTable;
 	@FXML
-	private TableColumn<FieldDTO, String> colFieldID,colFieldName,colFieldStatus,colDescription;
+	private TableColumn<FieldDTO, Integer> colFieldID;
+	@FXML
+	private TableColumn<FieldDTO, String> colFieldName,colFieldStatus,colDescription;
 	@FXML
 	private TableColumn<FieldDTO, BigDecimal> colPricePerHour;
+	
+	//Gọi lớp BLL để xử lý
+	private FieldBLL fieldBLL = new FieldBLL(new FieldDAL());
+	
+	//Gán dữ liệu cho table view
+	private ObservableList<FieldDTO> fieldList = FXCollections.observableArrayList();
+	
+	@FXML
+	public void initialize() {
+		//Cấu hình các cột table view
+		setupTableColumn();
+		
+		//Gọi dữ liệu ban đầu từ BLL
+		loadAllFields();
+		
+		//Khởi tạo trạng thái sân
+		loadComboBox();
+	}
+
+	private void loadComboBox() {
+	    fieldStatusComboBox.setItems(FXCollections.observableArrayList(FieldStatus.values()));
+	    // Thiết lập cho ComboBox để hiển thị giá trị status dưới dạng chuỗi
+	    fieldStatusComboBox.setCellFactory(param -> new javafx.scene.control.ListCell<FieldStatus>() {
+	        @Override
+	        protected void updateItem(FieldStatus item, boolean empty) {
+	            super.updateItem(item, empty);
+	            if (empty || item == null) {
+	                setText(null);
+	            } else {
+	                setText(item.getStatus());
+	            }
+	        }
+	    });
+	    fieldStatusComboBox.setButtonCell(fieldStatusComboBox.getCellFactory().call(null));
+	}
 
 
+	private void loadAllFields() {
+		colFieldID.setCellValueFactory(new PropertyValueFactory<>("fieldID"));
+		colFieldName.setCellValueFactory(new PropertyValueFactory<>("fieldName"));
+		colFieldStatus.setCellValueFactory(new PropertyValueFactory<>("statusString"));
+		colPricePerHour.setCellValueFactory(new PropertyValueFactory<>("pricePerHour"));
+		colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+	}
+
+	private void setupTableColumn() {
+		// Lấy danh sách sân bóng từ BLL
+		List<FieldDTO> fields = fieldBLL.getAllFields();
+		
+		// Làm mới dữ liệu trong table view
+		fieldList.setAll(fields);
+		fieldTable.setItems(fieldList);
+	}
+	
 }
