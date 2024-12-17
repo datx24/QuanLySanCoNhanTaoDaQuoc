1f58429adcc52579684d4d1b1b18454735160feb
datx24 <xuandat475@gmail.com>
2024-12-17 17:20:58 +0700

    cài đặt phương thức lấy tất cả các ccootj trong bảng field

--- QLSanBongDaQuoc_64130299/src/main/java/dat/nx/sanbongdaquoc/repositories/FieldDAL.java
+++ QLSanBongDaQuoc_64130299/src/main/java/dat/nx/sanbongdaquoc/repositories/FieldDAL.java
@@ -1,14 +1,30 @@
 package dat.nx.sanbongdaquoc.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import dat.nx.sanbongdaquoc.enums.*;
 import dat.nx.sanbongdaquoc.models.entities.*;
+import dat.nx.sanbongdaquoc.utils.DatabaseConnection;
 
 public class FieldDAL {
 	private FieldDTO fieldDTO = new FieldDTO();
 	private List<FieldDTO> fieldDTOs = new ArrayList<>();
+	private Connection connection;
+	
+	
+	public FieldDAL(Connection connection) {
+		try {
+			connection = DatabaseConnection.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	//Thêm sân
 	public boolean addField(FieldDTO field) {
 		return true;
@@ -30,6 +46,25 @@ public class FieldDAL {
 	
 	//Lấy tất cả sân bóng từ cơ sở dữ liệu
 	public List<FieldDTO> getAllFields() {
+		String query = "SELECT FieldID,FieldName,Status,PricePerHour,Description";
+		
+		try(PreparedStatement preparedStatement = connection.prepareStatement(query);
+			ResultSet resultSet = preparedStatement.executeQuery()) {
+			while(resultSet.next()) {
+				int fieldID = resultSet.getInt("FieldID");
+				String fieldName = resultSet.getString("FieldName");
+				String statusString = resultSet.getString("Status");
+				// chuyển đổi String sang enum
+				FieldStatus status = FieldStatus.valueOf(statusString.toUpperCase()); 
+				double pricePerHour	= resultSet.getDouble("PricePerHour");
+				String description = resultSet.getString("Description");
+				
+				fieldDTOs.add(new FieldDTO(fieldID,fieldName,status,pricePerHour,description));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return fieldDTOs;
 	}
 	
