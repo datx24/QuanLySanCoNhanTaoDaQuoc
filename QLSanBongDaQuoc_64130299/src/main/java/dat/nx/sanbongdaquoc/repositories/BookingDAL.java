296882cb6641ec4cbf2b4d1568fe8fcb0cf003d8
datx24 <xuandat475@gmail.com>
2024-12-24 19:11:46 +0700

    cài phương thức lấy tất cả thông tin bảng Booking

--- QLSanBongDaQuoc_64130299/src/main/java/dat/nx/sanbongdaquoc/repositories/BookingDAL.java
+++ QLSanBongDaQuoc_64130299/src/main/java/dat/nx/sanbongdaquoc/repositories/BookingDAL.java
@@ -1,15 +1,32 @@
 package dat.nx.sanbongdaquoc.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import dat.nx.sanbongdaquoc.enums.*;
 import dat.nx.sanbongdaquoc.models.entities.*;
+import dat.nx.sanbongdaquoc.utils.DatabaseConnection;
 
 public class BookingDAL {
 	private BookingDTO bookingDTO = new BookingDTO();
 	private List<BookingDTO> bookingDTOs = new ArrayList<>();
+	private Connection connection;
+	
+	
+	public BookingDAL() {
+		try {
+			this.connection = DatabaseConnection.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	// Thêm đơn đặt sân
 	public boolean addBooking(BookingDTO booking) {
 		return true;
@@ -27,8 +44,38 @@ public class BookingDAL {
 
 	// Lấy tất cả đơn đặt sân từ cơ sở dữ liệu
 	public List<BookingDTO> getAllBookings() {
-		return bookingDTOs;
+	    List<BookingDTO> listAllBookings = new ArrayList<>();
+	    String query = "SELECT " +
+	            "b.BookingID, " +
+	            "CONCAT(DATE_FORMAT(b.StartTime, '%H:%i'), ' - ', DATE_FORMAT(b.EndTime, '%H:%i'), ', ', DATE_FORMAT(b.BookingDate, '%d/%m/%Y')) AS TimeDetails, " +
+	            "b.Status, " +
+	            "b.PaymentStatus, " +
+	            "u.FullName, " +
+	            "f.FieldName " +
+	            "FROM bookings_64130299 b " +
+	            "JOIN users_64130299 u ON b.UserID = u.UserID " +
+	            "JOIN fields_64130299 f ON b.FieldID = f.FieldID";
+
+	    try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+	         ResultSet resultSet = preparedStatement.executeQuery()) {
+
+	        while (resultSet.next()) {
+	            BookingDTO bookingDTO = new BookingDTO();
+	            bookingDTO.setBookingID(resultSet.getString("BookingID"));
+	            bookingDTO.setTimeDetails(resultSet.getString("TimeDetails"));
+	            bookingDTO.setStatus(BookingStatus.fromString(resultSet.getString("Status")));
+	            bookingDTO.setPaymentStatus(PaymentStatus.fromString(resultSet.getString("PaymentStatus")));
+	            bookingDTO.setUserName(resultSet.getString("FullName"));
+	            bookingDTO.setFieldName(resultSet.getString("FieldName"));
+	            listAllBookings.add(bookingDTO);
+	        }
+	    } catch (SQLException e) {
+	        System.err.println("Error fetching bookings from database: " + e.getMessage());
+	        e.printStackTrace();
+	    }
+	    return listAllBookings;
 	}
+
 
 	// Lấy danh sách đặt sân theo trạng thái
 	public List<BookingDTO> getBookingByStatus(BookingStatus booking) {
